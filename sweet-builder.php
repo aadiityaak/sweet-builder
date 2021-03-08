f20b96dcec09cc5d7765e3b6bf3778b9ac6bd993
aadiityaak <aadiityaak@gmail.com>
2021-03-08 10:42:45 +0700

    Init

---
+++ sweet-builder.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Sweet Builder Modules
+ * Plugin URI: 
+ * Description: Speed up your project with sweet module.
+ * Version: 2.0.1
+ * Author: Velocity Developer
+ * Author URI: 
+ */
+define( 'FL_MODULE_EXAMPLES_DIR', plugin_dir_path( __FILE__ ) );
+define( 'FL_MODULE_EXAMPLES_URL', plugins_url( '/', __FILE__ ) );
+
+require_once FL_MODULE_EXAMPLES_DIR . 'classes/class-fl-custom-modules-example-loader.php';
